333b21739b554b9ff96bcd13e2041dbe0183940c
Xu Xiao Min <xmxu@leadstec.com.cn>
2026-03-17 20:13:55 +0800

    add dialog select

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,5 +1,8 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
+  console.log('block', block);
+  console.log('quoteWrapper', quoteWrapper);
+  console.log('quoteWrapper.textContent', quoteWrapper.textContent);
 
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
